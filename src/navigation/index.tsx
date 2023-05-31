1816459f1e48d891043f44ce71f0bcc3ccf4c0b1
chandra <chandrapenugonda655@gmail.com>
2023-05-31 06:07:46 +0530

    zustand persist global store

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-// import AuthStackNavigator from "./auth-stack-navigator";
+import AuthStackNavigator from "./auth-stack-navigator";
 import AppStackNavigator from "./app-stack-navigator";
+import useUserGlobalStore from "@/store/useUserGlobalStore";
 
 export default function Navigation() {
+  const { user, updateUser } = useUserGlobalStore();
+  useEffect(() => {
+    setTimeout(() => {
+      updateUser({ email: "test@test.com", name: "user" });
+    }, 5000);
+  }, []);
   return (
     <NavigationContainer>
-      {/* <AuthStackNavigator /> */}
-      <AppStackNavigator />
+      {user ? <AppStackNavigator /> : <AuthStackNavigator />}
     </NavigationContainer>
   );
 }
---
+++ src/store/useUserGlobalStore.ts
@@ -0,0 +1,24 @@
+import { IAuthenticatedUser } from "@/types";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+interface IUserGlobalStore {
+  user: IAuthenticatedUser | null;
+  updateUser: (user: IAuthenticatedUser | null) => void;
+}
+
+const useUserGlobalStore = create<IUserGlobalStore>()(
+  persist(
+    (set, get) => ({
+      user: null,
+      updateUser: (user) => set({ user }),
+    }),
+    {
+      name: "expo-todo-user-store",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
+
+export default useUserGlobalStore;
