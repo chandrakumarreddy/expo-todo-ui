85a74b82e9edb269a9ce4017961eea2fa54d267b
chandra <chandrapenugonda655@gmail.com>
2023-06-01 06:44:10 +0530

    confifure swr

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import { useFonts } from "expo-font";
 import { useCallback } from "react";
 import Navigation from "@/navigation";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { AppState } from "react-native";
+import { SWRConfig } from "swr";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -26,7 +28,33 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaProvider onLayout={onLayoutRootView}>
-        <Navigation />
+        <SWRConfig
+          value={{
+            provider: () => new Map(),
+            isVisible: () => true,
+            initFocus(callback) {
+              let appState = AppState.currentState;
+              const onAppStateChange = (nextAppState: any) => {
+                if (
+                  appState.match(/inactive|background/) &&
+                  nextAppState === "active"
+                ) {
+                  callback();
+                }
+                appState = nextAppState;
+              };
+              const subscription = AppState.addEventListener(
+                "change",
+                onAppStateChange
+              );
+              return () => {
+                subscription.remove();
+              };
+            },
+          }}
+        >
+          <Navigation />
+        </SWRConfig>
         <StatusBar translucent />
       </SafeAreaProvider>
     </ThemeProvider>
--- src/screens/bottom-tabs/categories/index.tsx
+++ src/screens/bottom-tabs/categories/index.tsx
@@ -1,11 +1,15 @@
-import { View, Text } from "react-native";
 import React from "react";
 import SafeAreaWrapper from "@/components/shared/safe-area-wrapper";
+import useSWR from "swr";
+import { fetcher } from "@/services/config";
+import { Text } from "@/utils/theme";
 
 export default function CategoriesScreen() {
+  const { data, isLoading } = useSWR("/api/v1/user/categories", fetcher);
+  console.log(!isLoading && data.categories);
   return (
     <SafeAreaWrapper>
-      <Text>categories</Text>
+      <Text variant="textSm">categories</Text>
     </SafeAreaWrapper>
   );
 }
